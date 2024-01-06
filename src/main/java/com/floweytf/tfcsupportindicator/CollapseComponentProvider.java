b00700948cb70bece08b4a368a3c771a9d396d54
FloweyTheFlower420 <guywithanaxe42@gmail.com>
2024-01-06 14:28:44 -0600

    feat: colored text and better collapse detection mechanism

--- src/main/java/com/floweytf/tfcsupportindicator/CollapseComponentProvider.java
+++ src/main/java/com/floweytf/tfcsupportindicator/CollapseComponentProvider.java
@@ -1,8 +1,12 @@
 package com.floweytf.tfcsupportindicator;
 
+import net.dries007.tfc.common.recipes.CollapseRecipe;
 import net.dries007.tfc.util.Support;
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
@@ -18,14 +22,31 @@ public enum CollapseComponentProvider implements IBlockComponentProvider {
     private static final ResourceLocation COLLAPSE_INDICATOR = new ResourceLocation("tfc_support_indicator:support_indicator");
     private static final Lazy<TagKey<Block>> CAN_START_COLLAPSE =
         Lazy.of(() -> TagKey.create(Registries.BLOCK, new ResourceLocation("tfc", "can_start_collapse")));
+    private static final Component SELF_SUPPORTED = Component.translatable("tfc_support_indicator.self_supported").withStyle(ChatFormatting.DARK_GREEN);
+    private static final Component SELF_UNSUPPORTED = Component.translatable("tfc_support_indicator.self_unsupported").withStyle(ChatFormatting.GOLD);
+    private static final Component WONT_TRIGGER_COLLAPSE = Component.translatable("tfc_support_indicator.wont_trigger_collapse").withStyle(ChatFormatting.DARK_GREEN);
+    private static final Component MIGHT_TRIGGER_COLLAPSE = Component.translatable("tfc_support_indicator.might_trigger_collapse").withStyle(ChatFormatting.RED);
+
 
     @Override
     public void appendTooltip(ITooltip tooltip, BlockAccessor accessor, IPluginConfig config) {
-        if(!accessor.getBlockState().is(CAN_START_COLLAPSE.get()))
+        if (!accessor.getBlockState().is(CAN_START_COLLAPSE.get()))
             return;
 
-        tooltip.add(Component.translatable(Support.isSupported(accessor.getLevel(), accessor.getPosition()) ?
-            "tfc_support_indicator.supported" : "tfc_support_indicator.not_supported" ));
+        var level = accessor.getLevel();
+        var pos = accessor.getPosition();
+
+        // the max collapse check radius is 4x2x4
+        boolean isSupported = Support.isSupported(level, accessor.getPosition());
+        boolean mightTriggerCollapse = Support.findUnsupportedPositions(
+                level,
+                pos.offset(-4, -2, -4),
+                pos.offset(4, 2, 4)
+            ).stream()
+            .anyMatch(u -> CollapseRecipe.canStartCollapse(level, u));
+
+        tooltip.add(isSupported ? SELF_SUPPORTED : SELF_UNSUPPORTED);
+        tooltip.add(mightTriggerCollapse ? MIGHT_TRIGGER_COLLAPSE : WONT_TRIGGER_COLLAPSE);
     }
 
     @Override
